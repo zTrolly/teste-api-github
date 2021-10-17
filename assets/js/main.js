c576be17e003116d6fb5510bad983478ddbebf68
Breno Lopes do Carmo <61760048+zTrolly@users.noreply.github.com>
2021-10-17 03:40:56 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+const URL = "https://api.github.com/users/GiroFC";
+const container = document.getElementById("container");
+var profileCard,profile,card;
+
+    function requestPerfil(URL, URL2){
+      fetch(URL)
+      .then(response => response.json())
+      .then(data => {
+        profile = data;
+      })
+      .catch(err => console.log(err));
+   }
+
+    function criandoPerfil(){
+      card = `
+        <div class="profile_picture">
+          <img src="${profile.avatar_url}">
+        </div>
+        <div class="profile_info">
+          <p>${profile.login}</p>
+        </div>
+      `
+      return card
+    }
+
+
+function startApp(URL){
+  requestPerfil(URL);
+  setTimeout(function () {
+    container.innerHTML = criandoPerfil();
+  }, 1000);
+}
+startApp(URL);
